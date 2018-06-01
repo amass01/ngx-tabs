2f3dd6fef6dcec164a82a8fd4c6bf5e0731eb732
Amir Massarwi <amassarwi@gmail.com>
2018-06-01 16:25:16 +0200

    set selected tab dynamically

--- src/tabs/tabs.component.ts
+++ src/tabs/tabs.component.ts
@@ -8,19 +8,27 @@ import { DataService } from '../common/data.service';
 })
 export class AppComponent {
 
-  // array to tabs metadata
+  // array of tabs metadata
   _tabs: any[];
+  _defaultTab: number;
 
   constructor(private dataService: DataService) {
 
     dataService.getTabs().subscribe(
-      (tabs) => this._tabs = tabs.tabs,
+      (tabs) => {
+        this._tabs = tabs.tabs;
+        this._defaultTab = 1;
+      },
       (err) => console.log(JSON.stringify(err))
     );
   }
 
   get tabs() {
     return this._tabs;
+  }
+
+  get defaultTab() {
+    return this._defaultTab;
   }
 
 
